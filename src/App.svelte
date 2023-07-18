70bfb9f429b25ed5d97ad9594ca3d5da651b8859
Marcel Martinez Otano <m.otano@omsag.de>
2023-07-18 17:09:00 +0200

    Add tailwind & WIP: CommentList

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+  import CommentList from "./lib/CommentList.svelte";
+
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
 </script>
 
-<Counter />
+<main>
+  <h1 class="text-2xl font-medium underline mb-5">Hallo</h1>
+
+  <CommentList />
+</main>
---
+++ src/lib/CommentList.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import CommentCard from "./CommentCard.svelte";
+  import { CommentStore } from "../stores";
+</script>
+
+{#each $CommentStore as comment (comment.id)}
+  {comment.id} - {comment.content} 
+{/each}
+<CommentCard />
