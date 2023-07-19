cf26db1654db4b234c47f9c36ab5c5cb6905a258
Marcel Martinez Otano <m.otano@omsag.de>
2023-07-19 08:49:33 +0200

    Fix Voter

--- src/lib/Voter.svelte
+++ src/lib/Voter.svelte
@@ -1,33 +1,19 @@
 <script lang="ts">
-	export let score: number = 0;
+  export let score: number = 0;
 </script>
 
 <div
-	class="flex h-10 items-center justify-around rounded-l-lg rounded-r-lg bg-very-light-gray"
+  class="flex h-10 items-center justify-around rounded-l-lg rounded-r-lg bg-very-light-gray"
 >
-	<!-- <div class="rounded-l-lg rounded-r-lg bg-very-light-gray"> -->
-	<!-- <button class="inline-flex h-10 w-10 items-center justify-center bg-red-500">
-		<img src="../../public/assets/images/icon-plus.svg" alt="Plus" />
-	</button>
-	<span
-		class="inline-flex h-10 w-8 items-center justify-center bg-blue-500 font-medium text-moderate-blue"
-		>{score}
-	</span>
-	<button
-		class="inline-flex h-10 w-10 items-center justify-center bg-green-500"
-	>
-		<img src="../../public/assets/images/icon-minus.svg" alt="Minus" />
-	</button> -->
-
-	<button class="p-3">
-		<img src="../../public/assets/images/icon-plus.svg" alt="Plus" />
-	</button>
-	<span
-		class="inline-flex min-w-[25px] justify-center font-medium text-moderate-blue"
-	>
-		{score}
-	</span>
-	<button class="p-3">
-		<img src="../../public/assets/images/icon-minus.svg" alt="Minus" />
-	</button>
+  <button class="inline-flex h-10 w-10 items-center justify-center">
+    <img src="/assets/images/icon-plus.svg" alt="+" />
+  </button>
+  <span
+    class="inline-flex min-w-[25px] justify-center font-medium text-moderate-blue"
+  >
+    {score}
+  </span>
+  <button class="inline-flex h-10 w-10 items-center justify-center">
+    <img src="/assets/images/icon-minus.svg" alt="-" />
+  </button>
 </div>
